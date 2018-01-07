0863bb1f67c17af123ff4ea707212001ec113d58
Administrator <Administrator@HQ1KMD1T9200>
2018-01-07 16:11:44 +0300

    Update VendorsImpl & VendorDAO :)

--- src/main/java/vendorsDLL/VendorDAO.java
+++ src/main/java/vendorsDLL/VendorDAO.java
@@ -1,10 +1,17 @@
 package vendorsDLL;
 
+import vendorsModel.Bank;
+import vendorsModel.Branch;
+import vendorsModel.ContactPerson;
 import vendorsModel.Vendor;
 
 public interface VendorDAO {
 	
 	
-public Integer AddVendor(Vendor vendor);	
+public Integer AddVendor(Vendor vendor);
+public Integer AddBank(Bank bank);
+public Integer AddContactPerson(ContactPerson contactPerson);
+
+public Integer AddBranch(Branch branch);
 
 }
--- src/main/java/vendorsDLL/VendorsImpl.java
+++ src/main/java/vendorsDLL/VendorsImpl.java
@@ -10,6 +10,9 @@ import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
+import vendorsModel.Bank;
+import vendorsModel.Branch;
+import vendorsModel.ContactPerson;
 import vendorsModel.Vendor;
 
 public class VendorsImpl implements VendorDAO {
@@ -52,6 +55,113 @@ public class VendorsImpl implements VendorDAO {
 	    				result.put("mobileNumber_in", vendor.mobileNumber);
 	    				result.put("webSiteurl_in", vendor.webSiteurl);
 	    				result.put("crNumber_in", vendor.crNumber);
+	    			 
+	    				
+	    			result = jdbcCall.execute(result);
+	    			
+	    			return (Integer) result.get("result");
+	  
+	             
+	    	}catch(Exception ex)
+	    	{
+	    		String exc=ex.getMessage();
+	    		
+	    		 return 0; 
+	    	}	
+	            
+	}
+	  
+	  
+	  public Integer AddBank(Bank bank) {
+	    	
+	    	try {
+	    		
+	    		SimpleJdbcCall jdbcCall = new 
+	                SimpleJdbcCall(dataSource).withProcedureName("AddBank")
+	                .withoutProcedureColumnMetaDataAccess().declareParameters(
+	                		new SqlParameter( "vendorID_in", Types.BIGINT ),
+	                		new SqlParameter( "bankName_in", Types.VARCHAR ),
+	                		new SqlParameter( "iban_in", Types.VARCHAR ),
+		    				new SqlOutParameter("result", Types.INTEGER ));
+	                       
+	
+	    				
+	    			Map<String, Object> result = new HashMap<String, Object>(2);
+	    				result.put("vendorID_in",bank.vendorID);
+	    				result.put("bankName_in", bank.bankName);
+	    				result.put("iban_in", bank.iban);
+	    			 
+	    				
+	    			result = jdbcCall.execute(result);
+	    			
+	    			return (Integer) result.get("result");
+	  
+	             
+	    	}catch(Exception ex)
+	    	{
+	    		String exc=ex.getMessage();
+	    		
+	    		 return 0; 
+	    	}	
+	            
+	}
+	  
+	  
+	  
+	  public Integer AddContactPerson(ContactPerson contactPerson) {
+	    	
+	    	try {
+	    		
+	    		SimpleJdbcCall jdbcCall = new 
+	                SimpleJdbcCall(dataSource).withProcedureName("AddContactPerson")
+	                .withoutProcedureColumnMetaDataAccess().declareParameters(
+	                		new SqlParameter( "vendorID_in", Types.BIGINT ),
+	                		new SqlParameter( "fullName_in", Types.VARCHAR ),
+	                		new SqlParameter( "mobile_in", Types.VARCHAR ),
+	                		new SqlParameter( "email_in", Types.VARCHAR ),
+		    				new SqlOutParameter("result", Types.INTEGER ));
+	                       
+	
+	    				
+	    			Map<String, Object> result = new HashMap<String, Object>(2);
+	    				result.put("vendorID_in",contactPerson.vendorID);
+	    				result.put("fullName_in", contactPerson.fullName);
+	    				result.put("mobile_in", contactPerson.mobile);
+	    				result.put("email_in", contactPerson.email);
+	    			 
+	    				
+	    			result = jdbcCall.execute(result);
+	    			
+	    			return (Integer) result.get("result");
+	  
+	             
+	    	}catch(Exception ex)
+	    	{
+	    		String exc=ex.getMessage();
+	    		
+	    		 return 0; 
+	    	}	
+	            
+	}
+	  
+	  
+	  
+	  public Integer AddBranch(Branch branch) {
+	    	
+	    	try {
+	    		
+	    		SimpleJdbcCall jdbcCall = new 
+	                SimpleJdbcCall(dataSource).withProcedureName("AddContactPerson")
+	                .withoutProcedureColumnMetaDataAccess().declareParameters(
+	                		new SqlParameter( "vendorID_in", Types.BIGINT ),
+	                		new SqlParameter( "cityID_in", Types.BIGINT ),
+		    				new SqlOutParameter("result", Types.INTEGER ));
+	                       
+	
+	    				
+	    			Map<String, Object> result = new HashMap<String, Object>(2);
+	    				result.put("vendorID_in",branch.vendorID);
+	    				result.put("cityID_in", branch.cityID);
 	    			 
 	    				
 	    			result = jdbcCall.execute(result);
